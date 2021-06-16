b191f0cd569ac2f521977e74062dd2a851b22c6b
Math W <mathewsto51@gmail.com>
2021-06-15 22:50:11 -0300

    build: change ts builds kk

--- commands/UpdateServerDatabase.ts
+++ commands/UpdateServerDatabase.ts
@@ -1,14 +1,14 @@
 import { BaseCommand } from '@adonisjs/core/build/standalone'
-import Client from '../app/Models/Client'
 import { BaseModel, LucidRow } from '@ioc:Adonis/Lucid/Orm'
-import Bank from '../app/Models/Helper/Bank'
-import PersonInfo from '../app/Models/PersonInfo'
-import Address from '../app/Models/Address'
-import FileItem from '../app/Models/FileItem'
-import FolderItem from '../app/Models/FolderItem'
-import User from '../app/Models/User'
-import BankInfo from '../app/Models/BankInfo'
-import Database from '@ioc:Adonis/Lucid/Database'
+// import Client from '../app/Models/Client'
+// import Bank from '../app/Models/Helper/Bank'
+// import PersonInfo from '../app/Models/PersonInfo'
+// import Address from '../app/Models/Address'
+// import FileItem from '../app/Models/FileItem'
+// import FolderItem from '../app/Models/FolderItem'
+// import User from '../app/Models/User'
+// import BankInfo from '../app/Models/BankInfo'
+// import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class UpdateServerDatabase extends BaseCommand {
   /**
@@ -44,7 +44,6 @@ export default class UpdateServerDatabase extends BaseCommand {
     // const fileItems = await this.all(FileItem)
     // const folderItems = await this.all(FolderItem)
     // const users = await this.all(User)
-
     // await this.migrate(clients[0], Client)
     // console.log('pers', personInfos[0].id)
     // for (const personInfo of personInfos) {
@@ -56,54 +55,45 @@ export default class UpdateServerDatabase extends BaseCommand {
     //     password: 'matrix@ltda',
     //   })
     // }
-
     // for (const folderItem of folderItems) {
     //   await this.migrate(folderItem, FolderItem)
     // }
-
     // for (const fileItem of fileItems) {
     //   await this.migrate(fileItem, FileItem)
     // }
-
     // for (const address of addresses) {
     //   await this.migrate(address, Address)
     // }
-
     // for (const bank of banks) {
     //   await this.migrate(bank, Bank)
     // }
-
     // for (const bankInfo of banksInfos) {
     //   await this.migrate(bankInfo, BankInfo)
     // }
-
     // for (const client of clients) {
     //   await this.migrate(client, Client)
     // }
-
-    const items = await Database.connection('sqlite').table('person_infos_has_folder_items')
-
-    await Database.connection('server').table('person_infos_has_folder_items').multiInsert(items)
-
-    this.logger.info('Hello world!')
+    // const items = await Database.connection('sqlite').table('person_infos_has_folder_items')
+    // await Database.connection('server').table('person_infos_has_folder_items').multiInsert(items)
+    // this.logger.info('Hello world!')
   }
 
   private all<T extends typeof BaseModel>(model: T) {
-    return model.all({
-      connection: 'sqlite',
-    })
+    // return model.all({
+    //   connection: 'sqlite',
+    // })
   }
 
   private migrate(item: LucidRow, model: typeof BaseModel, additionals = {}) {
-    const { created_at, updated_at, ...data } = item.toJSON()
-    return model.create(
-      {
-        ...data,
-        ...additionals,
-      },
-      {
-        connection: 'server',
-      }
-    )
+    // const { created_at, updated_at, ...data } = item.toJSON()
+    // return model.create(
+    //   {
+    //     ...data,
+    //     ...additionals,
+    //   },
+    //   {
+    //     connection: 'server',
+    //   }
+    // )
   }
 }
--- database/seeders/010EVisaMigrationSeeder.ts
+++ database/seeders/010EVisaMigrationSeeder.ts
@@ -24,189 +24,186 @@ export default class EVisaMigrationSeederSeeder extends BaseSeeder {
   private banks: Bank[] = []
 
   public async run() {
-    // console.log(AuthSys.user)
-    this.banks = await Bank.all()
-    const usersError: any[] = []
-    let count = 0
-    for (const user of data) {
-      count++
-      try {
-        const userInfos = await this.getInfos(user)
-        await this.fillDatabase(userInfos)
-
-        Logger.info(`User Integration [${count} - ${data.length}] [${userInfos.user.DT_RowId}]`)
-      } catch (e) {
-        usersError.push(user)
-        Logger.error(
-          `User Integration [${count} - ${data.length}] [${user.DT_RowId}][${user.nome}][${user.carteira}][${e}]`
-        )
-      }
-    }
-
-    fs.writeFileSync(Application.tmpPath('users-error.json'), JSON.stringify(usersError))
+    // this.banks = await Bank.all()
+    // const usersError: any[] = []
+    // let count = 0
+    // for (const user of data) {
+    //   count++
+    //   try {
+    //     const userInfos = await this.getInfos(user)
+    //     await this.fillDatabase(userInfos)
+    //     Logger.info(`User Integration [${count} - ${data.length}] [${userInfos.user.DT_RowId}]`)
+    //   } catch (e) {
+    //     usersError.push(user)
+    //     Logger.error(
+    //       `User Integration [${count} - ${data.length}] [${user.DT_RowId}][${user.nome}][${user.carteira}][${e}]`
+    //     )
+    //   }
+    // }
+    // fs.writeFileSync(Application.tmpPath('users-error.json'), JSON.stringify(usersError))
   }
 
-  private async fillDatabase(infos: any) {
-    const { user: userInfos, userAnexos, userConsulta } = infos
-    const username = StrHelper.title(userInfos.carteira).trim()
-    const user = User.firstOrCreate(
-      {
-        username,
-      },
-      {
-        email: StrHelper.slug(userInfos.carteira) + '@matrix.com',
-        isActive: false,
-        password: 'matrix@ltda',
-      }
-    )
+  // private async fillDatabase(infos: any) {
+  //   const { user: userInfos, userAnexos, userConsulta } = infos
+  //   const username = StrHelper.title(userInfos.carteira).trim()
+  //   const user = User.firstOrCreate(
+  //     {
+  //       username,
+  //     },
+  //     {
+  //       email: StrHelper.slug(userInfos.carteira) + '@matrix.com',
+  //       isActive: false,
+  //       password: 'matrix@ltda',
+  //     }
+  //   )
 
-    const phones: string[] = []
-    if (userConsulta.telefone) phones.push(StrHelper.digits(userConsulta.telefone))
-    if (userConsulta.telefone2) phones.push(StrHelper.digits(userConsulta.telefone2))
-    if (userConsulta.telefone3) phones.push(StrHelper.digits(userConsulta.telefone3))
+  //   const phones: string[] = []
+  //   if (userConsulta.telefone) phones.push(StrHelper.digits(userConsulta.telefone))
+  //   if (userConsulta.telefone2) phones.push(StrHelper.digits(userConsulta.telefone2))
+  //   if (userConsulta.telefone3) phones.push(StrHelper.digits(userConsulta.telefone3))
 
-    const personInfo = await PersonInfo.create({
-      phones,
-      cpf: StrHelper.digits(userConsulta.cpf),
-      firstName: StrHelper.title(userConsulta.nome).trim(),
-      genre: userConsulta.sexo,
-      fathersFullname: StrHelper.title(userConsulta.nome_pai).trim(),
-      mothersFullname: StrHelper.title(userConsulta.nome_mae).trim(),
-      dependentsNumber: Number(StrHelper.digits(userConsulta.n_dependentes)) || 0,
-      maritalStatus: Number(StrHelper.digits(userConsulta.estado_civil)),
-      educationalLevel: Number(StrHelper.digits(userConsulta.grau_instrucao)) || 0,
-      nacionality: StrHelper.title(userConsulta.nacionalidade).trim(),
-      naturalness: StrHelper.title(userConsulta.naturalidade).trim(),
-      state: (userConsulta.estado || '').toUpperCase().trim(),
-    })
+  //   const personInfo = await PersonInfo.create({
+  //     phones,
+  //     cpf: StrHelper.digits(userConsulta.cpf),
+  //     firstName: StrHelper.title(userConsulta.nome).trim(),
+  //     genre: userConsulta.sexo,
+  //     fathersFullname: StrHelper.title(userConsulta.nome_pai).trim(),
+  //     mothersFullname: StrHelper.title(userConsulta.nome_mae).trim(),
+  //     dependentsNumber: Number(StrHelper.digits(userConsulta.n_dependentes)) || 0,
+  //     maritalStatus: Number(StrHelper.digits(userConsulta.estado_civil)),
+  //     educationalLevel: Number(StrHelper.digits(userConsulta.grau_instrucao)) || 0,
+  //     nacionality: StrHelper.title(userConsulta.nacionalidade).trim(),
+  //     naturalness: StrHelper.title(userConsulta.naturalidade).trim(),
+  //     state: (userConsulta.estado || '').toUpperCase().trim(),
+  //   })
 
-    const bank = this.getBankByTitle(userConsulta.banco)
+  //   const bank = this.getBankByTitle(userConsulta.banco)
 
-    const conta = StrHelper.digits(userConsulta.conta)
-    const isCC = userConsulta.tipo_conta === 'CC'
-    const bankInfo = await BankInfo.create({
-      agency: StrHelper.digits(userConsulta.agencia),
-      cc: isCC ? conta : undefined,
-      cp: !isCC ? conta : undefined,
-      bankId: bank.id,
-      personInfoId: personInfo.id,
-    })
+  //   const conta = StrHelper.digits(userConsulta.conta)
+  //   const isCC = userConsulta.tipo_conta === 'CC'
+  //   const bankInfo = await BankInfo.create({
+  //     agency: StrHelper.digits(userConsulta.agencia),
+  //     cc: isCC ? conta : undefined,
+  //     cp: !isCC ? conta : undefined,
+  //     bankId: bank.id,
+  //     personInfoId: personInfo.id,
+  //   })
 
-    const address = await Address.create({
-      cep: StrHelper.digits(userConsulta.cep),
-      place: StrHelper.title(userConsulta.endereco),
-      number: userConsulta.numero.trim(),
-      state: (userConsulta.estado || '').toUpperCase().trim(),
-      city: StrHelper.title(StrHelper.html.decode(userConsulta.cidade).trim()),
-      neighborhood: StrHelper.title(StrHelper.html.decode(userConsulta.bairro).trim()),
-      complement: StrHelper.title(userConsulta.complemento).trim(),
-      personInfoId: personInfo.id,
-    })
+  //   const address = await Address.create({
+  //     cep: StrHelper.digits(userConsulta.cep),
+  //     place: StrHelper.title(userConsulta.endereco),
+  //     number: userConsulta.numero.trim(),
+  //     state: (userConsulta.estado || '').toUpperCase().trim(),
+  //     city: StrHelper.title(StrHelper.html.decode(userConsulta.cidade).trim()),
+  //     neighborhood: StrHelper.title(StrHelper.html.decode(userConsulta.bairro).trim()),
+  //     complement: StrHelper.title(userConsulta.complemento).trim(),
+  //     personInfoId: personInfo.id,
+  //   })
 
-    const client = await Client.create({
-      personInfoId: personInfo.id,
-      ownerId: user.id,
-      typeAgreement: 1,
-      isActive: true,
-    })
+  //   const client = await Client.create({
+  //     personInfoId: personInfo.id,
+  //     ownerId: user.id,
+  //     typeAgreement: 1,
+  //     isActive: true,
+  //   })
 
-    const folder = await FolderItem.create({
-      name: `${StrHelper.title(userConsulta.nome).trim()}`,
-      folderItemId: FoldersSys.folderUsers.id,
-    })
+  //   const folder = await FolderItem.create({
+  //     name: `${StrHelper.title(userConsulta.nome).trim()}`,
+  //     folderItemId: FoldersSys.folderUsers.id,
+  //   })
 
-    if (userAnexos.length > 0) {
-      for (const anexo of userAnexos) {
-        const { size, path } = await axios
-          .get<stream.Readable>(anexo, {
-            responseType: 'stream',
-          })
-          .then((res) => {
-            const size = res.headers['content-length']
-            const stream = res.data
+  //   if (userAnexos.length > 0) {
+  //     for (const anexo of userAnexos) {
+  //       const { size, path } = await axios
+  //         .get<stream.Readable>(anexo, {
+  //           responseType: 'stream',
+  //         })
+  //         .then((res) => {
+  //           const size = res.headers['content-length']
+  //           const stream = res.data
 
-            FileSystemHelper.createDirIfNotExists(Application.tmpPath('person'))
-            FileSystemHelper.createDirIfNotExists(Application.tmpPath('person', `${personInfo.id}`))
+  //           FileSystemHelper.createDirIfNotExists(Application.tmpPath('person'))
+  //           FileSystemHelper.createDirIfNotExists(Application.tmpPath('person', `${personInfo.id}`))
 
-            const path = ['person', `${personInfo.id}`, `${uuid.v4()}.jpg`]
+  //           const path = ['person', `${personInfo.id}`, `${uuid.v4()}.jpg`]
 
-            const writer = fs.createWriteStream(Application.tmpPath(...path))
-            stream.pipe(writer)
+  //           const writer = fs.createWriteStream(Application.tmpPath(...path))
+  //           stream.pipe(writer)
 
-            return {
-              size,
-              path: path.join('/'),
-            }
-          })
+  //           return {
+  //             size,
+  //             path: path.join('/'),
+  //           }
+  //         })
 
-        const file = await FileItem.create({
-          path,
-          mimeType: 'image/jpg',
-          folderItemId: folder.id,
-          size,
-          userId: AuthSys.user.id,
-          name: path,
-        })
-      }
-    }
+  //       const file = await FileItem.create({
+  //         path,
+  //         mimeType: 'image/jpg',
+  //         folderItemId: folder.id,
+  //         size,
+  //         userId: AuthSys.user.id,
+  //         name: path,
+  //       })
+  //     }
+  //   }
 
-    await folder.related('personInfos').attach([personInfo.id])
+  //   await folder.related('personInfos').attach([personInfo.id])
 
-    return client
-    // const personInfo = await PersonInfo.firstOrCreate({
-    //   email:
-    // })
-  }
+  //   return client
+  //   // const personInfo = await PersonInfo.firstOrCreate({
+  //   //   email:
+  //   // })
+  // }
 
-  private getBankByTitle(title: string) {
-    const titleSlug = StrHelper.slug(title)
-    const bank = this.banks.find((bank) => {
-      const nameSlug = StrHelper.slug(bank.name)
-      const fullNameSlug = StrHelper.slug(bank.fullName)
+  // private getBankByTitle(title: string) {
+  //   const titleSlug = StrHelper.slug(title)
+  //   const bank = this.banks.find((bank) => {
+  //     const nameSlug = StrHelper.slug(bank.name)
+  //     const fullNameSlug = StrHelper.slug(bank.fullName)
 
-      return nameSlug === titleSlug || fullNameSlug === titleSlug
-    })
-    if (!bank) throw new Error('Bank not found')
+  //     return nameSlug === titleSlug || fullNameSlug === titleSlug
+  //   })
+  //   if (!bank) throw new Error('Bank not found')
 
-    return bank
-  }
+  //   return bank
+  // }
 
-  private async getInfos(user: any) {
-    const pathEvisa = Application.tmpPath('evisa')
-    const pathFile = Application.tmpPath('evisa', `user-${user.DT_RowId}.json`)
+  // private async getInfos(user: any) {
+  //   const pathEvisa = Application.tmpPath('evisa')
+  //   const pathFile = Application.tmpPath('evisa', `user-${user.DT_RowId}.json`)
 
-    return CacheHelper.make(
-      async () => {
-        const userConsulta = await axios
-          .get(`http://crm.grupoevisa.com.br/data/clientes/consulta?c=${user.DT_RowId}`)
-          .then((res) => res.data)
-          .then((data) => data.cliente)
-          .then((clientes) => clientes[0])
+  //   return CacheHelper.make(
+  //     async () => {
+  //       const userConsulta = await axios
+  //         .get(`http://crm.grupoevisa.com.br/data/clientes/consulta?c=${user.DT_RowId}`)
+  //         .then((res) => res.data)
+  //         .then((data) => data.cliente)
+  //         .then((clientes) => clientes[0])
 
-        const userAnexos = await axios
-          .get<string>(`http://crm.grupoevisa.com.br/upload/lista-documentos?c=${user.DT_RowId}`)
-          .then((res) => res.data)
-          .then((html) => html.match(/src="(.*)"/gm))
-          .then(
-            (lines) =>
-              lines?.map((line) =>
-                line
-                  .replace(/src="/g, '')
-                  .replace(/"/g, '')
-                  .replace(/width=60/g, '')
-                  .trim()
-              ) || []
-          )
-          .then((urls) => urls.map((url) => `http://crm.grupoevisa.com.br/${url}`))
-          .catch(() => [] as string[])
+  //       const userAnexos = await axios
+  //         .get<string>(`http://crm.grupoevisa.com.br/upload/lista-documentos?c=${user.DT_RowId}`)
+  //         .then((res) => res.data)
+  //         .then((html) => html.match(/src="(.*)"/gm))
+  //         .then(
+  //           (lines) =>
+  //             lines?.map((line) =>
+  //               line
+  //                 .replace(/src="/g, '')
+  //                 .replace(/"/g, '')
+  //                 .replace(/width=60/g, '')
+  //                 .trim()
+  //             ) || []
+  //         )
+  //         .then((urls) => urls.map((url) => `http://crm.grupoevisa.com.br/${url}`))
+  //         .catch(() => [] as string[])
 
-        return { userConsulta, userAnexos, user }
-      },
-      {
-        dir: pathEvisa,
-        path: pathFile,
-        expiress: 1000 * 60 * 60 * 24 * 30,
-      }
-    )
-  }
+  //       return { userConsulta, userAnexos, user }
+  //     },
+  //     {
+  //       dir: pathEvisa,
+  //       path: pathFile,
+  //       expiress: 1000 * 60 * 60 * 24 * 30,
+  //     }
+  //   )
+  // }
 }
