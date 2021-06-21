8d0e528697bf22b25ad58f80e1934bca91dbdec3
Math W <mathewsto51@gmail.com>
2021-06-21 08:04:11 -0300

    lixo

--- app/Models/PersonInfo.ts
+++ app/Models/PersonInfo.ts
@@ -76,7 +76,7 @@ export default class PersonInfo extends BaseModel {
   public naturalness: string
 
   @column({
-    consume: (v) => (v ? JSON.parse(JSON.stringify(v)) : undefined),
+    consume: (v) => v,
     prepare: (v) => (v ? JSON.stringify(v) : undefined),
   })
   public phones: string[]
